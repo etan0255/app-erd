c277c37086e3d5652b5507dccd88554e41963f6a
etan0255 <touatyetan0@gmail.com>
2025-05-15 01:15:44 +0200

    Update config.php

--- config.php
+++ config.php
@@ -6,13 +6,15 @@ $pass = 'Etan2025!';
 $charset = 'utf8mb4';
 
 // Utilise le chemin relatif correct
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset;sslmode=require";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
 
 $options = [
     PDO::MYSQL_ATTR_SSL_CA    => __DIR__ . '/certs/DigiCert.pem',
     PDO::ATTR_ERRMODE         => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
+
 
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
