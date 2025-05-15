9ab3f1c8d824b0d7cff10b201fb6d3067d3c10b5
etan0255 <touatyetan0@gmail.com>
2025-05-16 00:53:10 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $host = 'mysql-app-erd-v2.mysql.database.azure.com';
 $db   = 'appdb';
 $user = 'adminuser@mysql-app-erd-v2';
-$pass = 'azerty11@';
+$pass = 'eTAN2025§';
 $charset = 'utf8mb4';
 
 // Utilise le chemin relatif correct
