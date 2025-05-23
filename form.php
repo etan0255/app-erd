4a41062105d96acfe56b00d7e5660abca1587ce7
etan0255 <touatyetan0@gmail.com>
2025-05-23 13:51:19 +0200

    Update form.php

--- form.php
+++ form.php
@@ -5,17 +5,11 @@
     <title>Formulaire de contact</title>
 </head>
 <body>
-    <h2>Formulaire de Contact</h2>
+    <h2>Ajouter un contact</h2>
     <form action="insert.php" method="POST">
-        <label>Nom :</label><br>
-        <input type="text" name="nom" required><br>
-
-        <label>Email :</label><br>
-        <input type="email" name="email" required><br>
-
-        <label>Message :</label><br>
-        <textarea name="message" required></textarea><br>
-
+        Nom : <input type="text" name="nom" required><br>
+        Email : <input type="email" name="email" required><br>
+        Message : <textarea name="message" required></textarea><br>
         <button type="submit">Envoyer</button>
     </form>
 </body>
